8e9ad55161de907dd14ba5b88e106d7a70e3bb24
nnalinn <alina.vysotskayaaa@gmail.com>
2025-03-06 23:43:29 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,53 @@
 import './App.scss';
-import { Component } from 'react';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [query, setQuery] = useState('');
 
-    this.state = {
-      query: '',
-    };
-  }
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
 
-  handleInputChange(event) {
-    this.setState({ query: event.target.value });
-  }
-
-  render() {
-    const { query } = this.state;
-
-    const visibleMovies = moviesFromServer.filter(movie => {
-      const normalizedQuery = query.trim().toLowerCase();
-
-      return (
-        movie.title.toLowerCase().includes(normalizedQuery) ||
-        movie.description.toLowerCase().includes(normalizedQuery)
-      );
-    });
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const normalizedQuery = query.trim().toLowerCase();
 
     return (
-      <div className="page">
-        <div className="page-content">
-          <div className="box">
-            <div className="field">
-              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-              <label htmlFor="search-query" className="label">
-                Search movie
-              </label>
+      movie.title.toLowerCase().includes(normalizedQuery) ||
+      movie.description.toLowerCase().includes(normalizedQuery)
+    );
+  });
 
-              <div className="control">
-                <input
-                  type="text"
-                  id="search-query"
-                  className="input"
-                  placeholder="Type search word"
-                  value={query}
-                  onChange={this.handleInputChange}
-                />
-              </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={handleInputChange}
+              />
             </div>
           </div>
-
-          <MoviesList movies={visibleMovies} />
         </div>
 
-        <div className="sidebar">Sidebar goes here</div>
+        <MoviesList movies={visibleMovies} />
       </div>
-    );
-  }
-}
+
+      <div className="sidebar">Sidebar goes here</div>
+    </div>
+  );
+};
+
+export default App;
